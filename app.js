efd1027235c327823d1f8dd006201661f204ea8e
Leandro Rey <leandrodrey@gmail.com>
2024-06-22 17:46:19 -0300

    Create initial express server in app.js
    This commit adds a basic express server setup in app.js. It includes a GET endpoint at the root, a catch-all for non existent routes that returns a 404 status, and it listens on a specified or default port.

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const PORT = process.env.PORT ?? 3000;
+
+const app = express();
+app.disable('x-powered-by');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.use((req, res) => {
+    res.status(404).send('Not Found');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+});
