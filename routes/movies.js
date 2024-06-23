a16bbdb94696c6e9c4b61823775c5108efc4983a
Leandro Rey <leandrodrey@gmail.com>
2024-06-23 02:50:07 -0300

    Refactor movie routes to use controller
    The movie routes have been significantly refactored to remove inline function definitions and instead utilize a dedicated MoviesController. This change allows for better code organization and reusability, making it easier to maintain and understand.

--- routes/movies.js
+++ routes/movies.js
@@ -1,52 +1,10 @@
 import {Router} from 'express';
-import {validateMovie, validatePartialMovie} from "../schemas/movies.js";
-import {MovieModel} from "../models/movie.js";
+import {MoviesController} from "../controllers/movies.js";
 
 export const moviesRouter = Router();
 
-moviesRouter.get('/', async (req, res) => {
-    const {genre} = req.query;
-    const movies = await MovieModel.getAll({genre});
-    res.json(movies);
-});
-
-moviesRouter.get('/:id', async (req, res) => {
-    const {id} = req.params;
-    const movie = await MovieModel.getById(id);
-    if (!movie) {
-        return res.status(404).send('Movie not found');
-    }
-    res.json(movie);
-});
-
-moviesRouter.post('/', async (req, res) => {
-    const result = validateMovie(req.body);
-    if (result.error) {
-        return res.status(400).json({error: JSON.parse(result.error.message)});
-    }
-    const newMovie = await MovieModel.create({input: result.data});
-    res.status(201).json(newMovie);
-});
-
-moviesRouter.delete('/:id', async (req, res) => {
-    const {id} = req.params;
-    const result = await MovieModel.delete({id});
-    if (!result) {
-        return res.status(404).send('Movie not found');
-    }
-    res.send('Movie deleted');
-});
-
-moviesRouter.patch('/:id', async (req, res) => {
-    const {id} = req.params;
-    const result = validatePartialMovie(req.body);
-    if (result.error) {
-        return res.status(400).json({error: JSON.parse(result.error.message)});
-    }
-
-    const updatedMovie = await MovieModel.update({id, input: result.data});
-    if (!updatedMovie) {
-        return res.status(404).send('Movie not found');
-    }
-    res.json(updatedMovie);
-});
+moviesRouter.get('/', MoviesController.getAll);
+moviesRouter.get('/:id', MoviesController.getById);
+moviesRouter.post('/', MoviesController.create);
+moviesRouter.delete('/:id', MoviesController.delete);
+moviesRouter.patch('/:id', MoviesController.update);
