d2975a98accc17198146fd64154bec1fcc3ad371
Leandro Rey <leandrodrey@gmail.com>
2024-06-28 20:09:44 -0300

    Swagger UI setup

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 import express, {json} from 'express';
 import {corsMiddleware} from "./middlewares/cors.js";
-import {docRouter} from './routes/api-docs.js';
+/*import {docRouter} from './routes/api-docs.js';*/
 import {createMoviesRouter} from './routes/movies.js';
 import {MovieModel} from "./models/mysql/movie.js";
+import swaggerUi from 'swagger-ui-express';
+import { openapiSpecification } from './swagger.js'
+
+let options = {
+    swaggerOptions: {
+        validatorUrl: null
+    }
+};
 
 const app = express();
 
-app.use('/api-docs', docRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification, options));
+/*app.use('/api-docs', docRouter);*/
 app.disable('x-powered-by');
 app.use(json());
 app.use(corsMiddleware());
