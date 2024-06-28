c25b09278b2771b654470e5520111fe054b8347c
Leandro Rey <leandrodrey@gmail.com>
2024-06-28 18:43:48 -0300

    Add dotenv to project and adjust swagger settings

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
+/*import dotenv from 'dotenv'*/
 import express, {json} from 'express';
 import {corsMiddleware} from "./middlewares/cors.js";
 import {createMoviesRouter} from './routes/movies.js';
 import {MovieModel} from "./models/mysql/movie.js";
-import swaggerUi from 'swagger-ui-express';
-import { openapiSpecification } from './swagger.js';
+/*import swaggerUi from 'swagger-ui-express';
+import { openapiSpecification } from './swagger.js';*/
+
+dotenv.config()
 
 const app = express();
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 
 app.disable('x-powered-by');
 app.use(json());
 app.use(corsMiddleware());
 app.use('/movies', createMoviesRouter({movieModel: MovieModel}));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 
 const PORT = process.env.PORT ?? 3000;
 
