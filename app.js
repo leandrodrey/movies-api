3471d5f8227f0b55158b8607e103f42eced59604
leandrodrey <leandrodrey@gmail.com>
2024-07-01 17:56:47 -0300

    Add user authentication endpoints

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
-import express, {json} from 'express';
-import {corsMiddleware} from "./middlewares/cors.js";
-import {docRouter} from './routes/api-docs.js';
-import {createMoviesRouter} from './routes/movies.js';
-import {MovieModel} from "./models/mysql/movie.js";
+import express, { json } from 'express'
+import { corsMiddleware } from './middlewares/cors.js'
+import { docRouter } from './routes/api-docs.js'
+import { createMoviesRouter } from './routes/movies.js'
+import { MovieModel } from './models/mysql/movie.js'
 
-const app = express();
+const app = express()
 
-app.disable('x-powered-by');
+app.disable('x-powered-by')
 
-app.use(json());
-app.use(corsMiddleware());
-app.use('/api-docs', docRouter);
-app.use('/movies', createMoviesRouter({movieModel: MovieModel}));
+app.use(json())
+app.use(corsMiddleware())
+app.use('/api-docs', docRouter)
+app.use('/movies', createMoviesRouter({ movieModel: MovieModel }))
 
-const PORT = process.env.PORT ?? 3000;
+app.post('/login', (req, res) => {
+})
+
+app.post('/register', (req, res) => {
+})
+
+app.post('/logout', (req, res) => {
+})
+
+app.get('/protected', (req, res) => {
+})
+
+const PORT = process.env.PORT ?? 3000
 
 app.listen(PORT, () => {
-    console.log(`Server started on ${PORT}`);
-});
+  console.log(`Server started on ${PORT}`)
+})
 
 app.use((req, res) => {
-    res.status(404).send('Not Found');
-});
+  res.status(404).send('Not Found')
+})
