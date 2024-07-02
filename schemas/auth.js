ad944d715e8c0e4fa09453f41ac6ba19b54778d6
Leandro Rey <leandrodrey@gmail.com>
2024-07-02 19:35:53 -0300

    Add user validation schema

---
+++ schemas/auth.js
@@ -0,0 +1,16 @@
+import z from 'zod'
+
+const userSchema = z.object({
+  name: z.string().min(2),
+  lastname: z.string().min(2),
+  email: z.string().email(),
+  password: z.string().min(6),
+  birthdate: z.string().regex(/^\d{4}-\d{2}-\d{2}$/),
+  country: z.string().min(2),
+})
+
+function validateUser (user) {
+  return userSchema.safeParse(user)
+}
+
+export { validateUser }
