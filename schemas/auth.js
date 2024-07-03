f8c22b8f9c2fa4e1db88a5b56b273c6a52e8ea97
Leandro Rey <leandrodrey@gmail.com>
2024-07-03 15:44:20 -0300

    Add partial user validation to auth schema
    The commit adds a function to validate partial users in the authentication schema. Previously, only full user objects could be validated, this update enables validation even for partially complete user objects.

--- schemas/auth.js
+++ schemas/auth.js
@@ -1,16 +1,20 @@
 import z from 'zod'
 
 const userSchema = z.object({
-  name: z.string().min(2),
-  lastname: z.string().min(2),
-  email: z.string().email(),
-  password: z.string().min(6),
-  birthdate: z.string().regex(/^\d{4}-\d{2}-\d{2}$/),
-  country: z.string().min(2),
+    name: z.string().min(2),
+    lastname: z.string().min(2),
+    email: z.string().email(),
+    password: z.string().min(6),
+    birthdate: z.string().regex(/^\d{4}-\d{2}-\d{2}$/),
+    country: z.string().min(2),
 })
 
 function validateUser (user) {
-  return userSchema.safeParse(user)
+    return userSchema.safeParse(user)
 }
 
-export { validateUser }
+function validatePartialUser (user) {
+    return userSchema.partial().safeParse(user)
+}
+
+export { validateUser, validatePartialUser }
