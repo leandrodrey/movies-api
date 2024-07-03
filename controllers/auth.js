aec3125d741a72fafbfa608edc97fcd979b7e477
Leandro Rey <leandrodrey@gmail.com>
2024-07-03 15:44:49 -0300

    Improve login and registration validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,30 +1,58 @@
-import { validateUser } from '../schemas/auth.js'
+import { validatePartialUser, validateUser } from '../schemas/auth.js'
+import jwt from 'jsonwebtoken'
 
 export class AuthController {
 
-    constructor({authModel}) {
-        this.authModel = authModel;
+    constructor ({ authModel }) {
+        this.authModel = authModel
     }
 
     login = async (req, res) => {
-    }
-
-    register = async (req, res) =>{
-        const result = validateUser(req.body);
+        const result = validatePartialUser(req.body)
         if (result.error) {
-            return res.status(400).json({error: JSON.parse(result.error.message)});
+            return res.status(400).json({ error: JSON.parse(result.error.message) })
         }
         try {
-            const isUserExists = await this.authModel.isUserExists(result.email);
-            if (isUserExists) {
-                return res.status(400).json({error: 'User already exists'});
+            const isUserExists = await this.authModel.isUserExists({ email: result.data.email })
+            if (!isUserExists) {
+                return res.status(401).json({ error: 'User doesnt exist' })
             }
-            const user = await this.authModel.register(result);
+            const isPasswordCorrect = await this.authModel.isPasswordCorrect({ user: result.data })
+            if (!isPasswordCorrect) {
+                return res.status(401).json({ error: 'Invalid credentials' })
+            }
+            const user = await this.authModel.login({ email: result.data.email })
+            const token = jwt.sign({ user }, process.env.JWT_SECRET ?? 'secret test to use in local', { expiresIn: '1h' })
             if (user) {
-                return res.status(201).json(user);
+                res.status(200).json({ user, token });
+            } else {
+                return res.status(401).json({ error: 'Invalid credentials' })
             }
         } catch (error) {
-            res.status(500).send('Internal Server Error')
+            console.error('Login failed for an unknown reason:', error)
+            return res.status(500).json({ error: 'Login failed' })
+        }
+    }
+
+    register = async (req, res) => {
+        const result = validateUser(req.body)
+        if (result.error) {
+            return res.status(400).json({ error: JSON.parse(result.error.message) })
+        }
+        try {
+            const isUserExists = await this.authModel.isUserExists({ email: result.data.email })
+            if (isUserExists) {
+                return res.status(400).json({ error: 'User already exists' })
+            }
+            const user = await this.authModel.register({ user: result.data })
+            if (user) {
+                return res.status(201).json(user)
+            } else {
+                console.error('Registration failed for an unknown reason')
+                return res.status(500).json({ error: 'User creation failed' })
+            }
+        } catch (error) {
+            res.status(500).send('Registration failed for an unknown reason')
         }
     }
 
