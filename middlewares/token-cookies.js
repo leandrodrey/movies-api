c77b82d5ae45ea6a1d6a9bb9106dae7e17fcf2d0
Leandro Rey <leandrodrey@gmail.com>
2024-07-04 18:15:40 -0300

    Remove express-session dependency and related usage

--- middlewares/token-cookies.js
+++ middlewares/token-cookies.js
@@ -2,6 +2,8 @@ import jwt from 'jsonwebtoken'
 
 export const tokenCookie = () => (req, res, next) => {
     const token = req.cookies['access_token']
+    req.session = { user: null }
+
     try {
         const data = jwt.verify(token, process.env.JWT_SECRET)
         req.session.user = data.user
